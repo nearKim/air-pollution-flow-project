f95270b176b212d53bb296c087218a63cfca156b
nearKim <garfield@toss.im>
2021-11-15 15:52:54 +0900

    service refac

--- services/api.py
+++ services/api.py
@@ -1,5 +1,6 @@
 import json
 import typing
+from datetime import datetime
 from functools import lru_cache
 
 import requests
@@ -13,9 +14,12 @@ API_ROOT = f"http://openAPI.seoul.go.kr:8088/{API_KEY}/json/TimeAverageAirQualit
 
 
 class APIService:
+    DATE_FORMAT = "%Y%m%d"
+
     @lru_cache(maxsize=None)
-    def result_count(self, target_date_str: str):
-        base_url = self.get_api_url(target_date_str, 1, 1)
+    def get_result_count(self, target_datetime: datetime):
+        target_datetime_str = target_datetime.strftime(self.DATE_FORMAT)
+        base_url = self.get_api_url(target_datetime_str, 1, 1)
         r = requests.get(base_url)
         assert r.status_code == 200
 
@@ -27,21 +31,11 @@ class APIService:
         url = f"{API_ROOT}/{start_idx}/{end_idx}/{target_date_str}"
         return url
 
-    def get_fullday_api_url(self, target_date_str: str) -> str:
-        result_count = self.result_count(target_date_str)
-        url = self.get_api_url(target_date_str, 1, result_count)
-        return url
-
     def get_air_quality_list(
-        self, target_date_str: str, start_idx: int, end_idx: int
+        self, target_datetime: datetime, start_idx: int, end_idx: int
     ) -> typing.List[AirQualityDTO]:
-        url = self.get_api_url(target_date_str, start_idx, end_idx)
-        return request_data(url)
-
-    def get_fullday_air_quality_list(
-        self, target_date_str: str
-    ) -> typing.List[AirQualityDTO]:
-        url = self.get_fullday_api_url(target_date_str)
+        target_datetime_str = target_datetime.strftime(self.DATE_FORMAT)
+        url = self.get_api_url(target_datetime_str, start_idx, end_idx)
         return request_data(url)
 
 
