35c3c586afde20f196f0d4403afa8611526c295c
nearKim <garfield@toss.im>
2022-01-30 14:55:07 +0900

    [tmp] db connection test dag 추가

---
+++ dags/example_db_conn.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime, timedelta
+from time import sleep
+
+from airflow import DAG
+
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+from infra.db import engine
+
+
+def use_connection(*args, **kwargs):
+    with engine.connect() as conn:
+        sleep(60)
+        print(f"sleeping from {os.getpid()} \n", flush=True)
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+}
+
+with DAG(
+    "example_db_conn",
+    default_args=default_args,
+    description="db conn test",
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    max_active_runs=100,
+    tags=["example"],
+) as dag:
+    start = DummyOperator(task_id="start")
+    t1 = PythonOperator(task_id="use_conn", python_callable=use_connection)
+    end = DummyOperator(task_id="end")
+
+    start >> t1 >> end
